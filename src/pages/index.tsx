f2befb8f111d4c997dce6991845f6bdc715b2a5b
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-02-28 19:18:24 +0100

    🎨 M- added Countdownprovider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import CompletedChallenges from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
 import ChallengeBox from "../components/ChallengeBox";
 import styles from '../styles/pages/Home.module.css';
+import { CountDownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -13,16 +14,18 @@ export default function Home() {
       <title>Início | move.it</title>
     </Head>
       <ExperienceBar/>
-      <section>
-        <div>
-          <Profile/>
-          <CompletedChallenges/>
-          <Countdown/>
-        </div>
-        <div>
-          <ChallengeBox/>
-        </div>
-      </section>
+      <CountDownProvider>
+        <section>
+          <div>
+            <Profile/>
+            <CompletedChallenges/>
+            <Countdown/>
+          </div>
+          <div>
+            <ChallengeBox/>
+          </div>
+        </section>
+      </CountDownProvider>
     </div>
   )
 }
