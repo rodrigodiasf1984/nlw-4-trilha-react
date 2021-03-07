e8d850ff5b773958f549fc8bd324958a379b44c8
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-03-07 16:37:32 +0100

    🎨 M- added js-cookie

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d2fc6ed052cd8375e2f2";
+/******/ 	var hotCurrentHash = "496af8a20f360ead0649";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
