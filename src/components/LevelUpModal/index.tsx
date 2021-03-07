447a96a9ba8ffc50f47abd01c63f6d06c4173c1a
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-03-07 16:40:48 +0100

    🎨 M- create a modal to show the level up for the user

---
+++ src/components/LevelUpModal/index.tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/ChallengeContext';
+import styles from '../../styles/components/LevelUpModal.module.css';
+
+const LevelUpModal: React.FC = () => {
+  const {level, closeLevelUpModal} = useContext(ChallengesContext);
+  return(
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <header>{level}</header>
+        <strong>Parabés</strong>
+        <p>Você alcançou um novo level.</p>
+        <button type="button" onClick={closeLevelUpModal}>
+          <img src="/icons/close.svg" alt="Fechar modal"/>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default LevelUpModal;
