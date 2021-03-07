053ff2784a438c1558685a8c16441b88cb571aa8
Rodrigo Dias De Figueiredo <rodrigodiasf@gmail.com>
2021-03-07 16:42:21 +0100

    🎨 M-added challenge provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import {GetServerSideProps} from 'next';
 import ExperienceBar  from "../components/ExperienceBar";
 import Profile from "../components/Profile";
 import CompletedChallenges from "../components/CompletedChallenges";
@@ -6,26 +7,48 @@ import Countdown from "../components/Countdown";
 import ChallengeBox from "../components/ChallengeBox";
 import styles from '../styles/pages/Home.module.css';
 import { CountDownProvider } from "../contexts/CountdownContext";
+import {ChallengesProvider} from '../contexts/ChallengeContext';
+import {ChallengesProviderProps} from '../@types';
 
-export default function Home() {
+export default function Home(props:ChallengesProviderProps) {
+  
   return (
-    <div className={styles.container}>     
-    <Head>
-      <title>Início | move.it</title>
-    </Head>
-      <ExperienceBar/>
-      <CountDownProvider>
-        <section>
-          <div>
-            <Profile/>
-            <CompletedChallenges/>
-            <Countdown/>
-          </div>
-          <div>
-            <ChallengeBox/>
-          </div>
-        </section>
-      </CountDownProvider>
-    </div>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >      
+      <div className={styles.container}>     
+      <Head>
+        <title>Início | move.it</title>
+      </Head>       
+        <ExperienceBar/>
+        <CountDownProvider>
+          <section>
+            <div>
+              <Profile/>
+              <CompletedChallenges/>
+              <Countdown/>
+            </div>
+            <div>
+              <ChallengeBox/>
+            </div>
+          </section>
+        </CountDownProvider>
+      </div>
+    </ChallengesProvider> 
   )
+}
+
+export const getServerSideProps:GetServerSideProps = async(ctx) => {
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+         
+  
+  return {
+    props: {
+      level:Number(level), 
+      currentExperience:Number(currentExperience), 
+      challengesCompleted:Number(challengesCompleted) 
+    }
+  }
 }
